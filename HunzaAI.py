6da3cb3e957191ab008fde5e82c3ef4b48f16cbe
alihassanoo <alihassanm381@gmail.com>
2024-08-28 20:15:25 +0500

    button consistency added

--- HunzaAI.py
+++ HunzaAI.py
@@ -180,24 +180,6 @@ with st.container():
             if error_message:
                 st.markdown(f'<div style="color: red; font-weight: bold;">{error_message}</div>', unsafe_allow_html=True)
 
-            # Align the button and ensure it has the same styling as the "Previous" button
-            st.markdown("""
-                <style>
-                div.stButton > button {
-                    background: linear-gradient(90deg, #FF490E 0%, #FF7B02 100%) !important;
-                    border: none;
-                    color: white;
-                    padding: 10px 20px;
-                    text-align: center;
-                    display: inline-block;
-                    font-size: 16px;
-                    margin: 4px 2px;
-                    cursor: pointer;
-                    border-radius: 8px;
-                }
-                </style>
-            """, unsafe_allow_html=True)
-
             next_clicked = st.form_submit_button('Next')
             
             if next_clicked:
